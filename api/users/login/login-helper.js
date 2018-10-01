9b7dabace214c6cf98d2ebc8417ed42c5e2324bc
Belmin Mustajbasic <belmin.mustajbasic@gmail.com>
2018-10-01 23:14:40 +0200

    Made some changes to cause conflicts

--- api/users/login/login-helper.js
+++ api/users/login/login-helper.js
@@ -1,23 +1,22 @@
 const { users } = require('../../../utility/mock-data');
 const signIn = (username, password) => {
     let validUser;
-    let msg={
-        username:'invalid',
-        password: 'invalid'
-    };
     validUser = users.find((user) => {
         return user.username === username;
     });
+    if (!validUser) {
+        return {
+            error: 'Invalid username',
+        }
+    }
 
-    if(validUser){
-        msg.username='valid';
-    }else{return msg;}
+    if (validUser.password !== password) {
+        return {
+            error: 'Invalid password',
+        }
+    }
 
-    if(validUser && validUser.password===password){
-        msg.password='valid';
-    }else {return msg;}
-
-    return msg;
+    return validUser;
 };
 
 module.exports = { signIn };
